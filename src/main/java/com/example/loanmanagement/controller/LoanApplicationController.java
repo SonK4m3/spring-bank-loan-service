4a5ed25a797af5d35b74d97de4b0bb8da7799780
SonK4m3 <nguyenhongsont1lhp@gmail.com>
2024-03-25 11:21:21 +0700

    feat: add interest calculation route in controller

--- src/main/java/com/example/loanmanagement/controller/LoanApplicationController.java
+++ src/main/java/com/example/loanmanagement/controller/LoanApplicationController.java
@@ -1,7 +1,10 @@
 package com.example.loanmanagement.controller;
 
+import com.example.loanmanagement.entity.EInterestCalculator;
 import com.example.loanmanagement.entity.LoanApplication;
+import com.example.loanmanagement.model.payload.response.MessageResponse;
 import com.example.loanmanagement.service.LoanApplicationService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -45,5 +48,12 @@ public class LoanApplicationController {
     public ResponseEntity<Void> deleteLoanApplication(@PathVariable Long id) {
         loanApplicationService.deleteLoanApplication(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/interest/{id}")
+    public ResponseEntity<MessageResponse> interestCalculation(@PathVariable Long id, @PathParam("type") EInterestCalculator type) {
+        double interest = loanApplicationService.calculateInterest(id, type);
+        MessageResponse messageResponse = new MessageResponse("" + interest);
+        return new ResponseEntity<>(messageResponse, HttpStatus.OK);
     }
 }
